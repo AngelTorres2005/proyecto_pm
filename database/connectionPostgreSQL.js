dc12fe09bbbef140a970e8e501223a908e670897
AngelTorres2005 <m386bbb14c651@gmail.com>
2026-02-24 13:01:24 -0600

    database cambiado

--- database/connectionPostgreSQL.js
+++ database/connectionPostgreSQL.js
@@ -1,11 +1,10 @@
 const pg = require('pg');
-//configuracion de la conexion a sql
+const dotenv = require('dotenv')
+
+dotenv.config();
+
 const pool=new pg.Pool({
-    host:'localhost',
-    port:'5432',
-    database:'proyecto_pm',
-    user:'postgres',
-    password:'Aggtcraft1'
+    connectionString: process.env.DATABASE_URL
 });
 
 module.exports = { pool };
